55275320e226ce9caa9a7ae255a35cc450d8b52d
SmaVivian <vivi163abc@163.com>
2020-07-01 18:26:04 +0800

    框架结构整理，新增文件夹,封装接口请求，新增index示例

---
+++ src/utils/util.js
@@ -0,0 +1,55 @@
+/**
+ * parseTime(time, '{y}-{m}-{d}')
+ * parseTime(time, '{y}-{m}-{d} {h}:{i}:{s}')
+ */
+function parseTime(time, cFormat) {
+  if (arguments.length === 0) {
+    return null
+  }
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if (('' + time).length === 10) time = parseInt(time) * 1000
+    date = new Date(time)
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    let value = formatObj[key]
+    // Note: getDay() returns 0 on Sunday
+    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
+    if (result.length > 0 && value < 10) {
+      value = '0' + value
+    }
+    return value || 0
+  })
+  return time_str
+}
+
+function getCurrentTime() {
+  let keep = ''
+  let date = new Date()
+  let y = date.getFullYear()
+  let m = date.getMonth() + 1
+  m = m < 10 ? '0' + m : m
+  let d = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+  let h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+  let f = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+  let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+  keep = y + '' + m + '' + d + '' + h + '' + f + '' + s
+  return keep // 20160614134947
+}
+
+module.exports = {
+  parseTime: parseTime,
+  getCurrentTime: getCurrentTime,
+}
---
+++ src/utils/wxRequest.js
@@ -0,0 +1,31 @@
+import wepy from '@wepy/core'
+import util from './util';
+// import md5 from './md5';
+import tip from './tip'
+import promisify  from '@wepy/use-promisify'
+wepy.use(promisify)
+
+const API_SECRET_KEY = 'www.mall.cycle.com'
+const TIMESTAMP = util.getCurrentTime()
+// const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
+
+const wxRequest = async(url, params = {}) => {
+    tip.loading();
+    let data = params.query || {};
+    // data.sign = SIGN;
+    data.time = TIMESTAMP;
+    let res = await wepy.wx.request({
+      url: url,
+      method: params.method || 'GET',
+      data: data,
+      header: { 'Content-Type': 'application/json' },
+    });
+    console.log(222, res)
+    tip.loaded();
+    return res.data;
+};
+
+
+module.exports = {
+  wxRequest
+}
